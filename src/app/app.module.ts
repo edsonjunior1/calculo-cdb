75c269af073e33e4c1fc0f4e11c0f8965a40b969
Edson Ascendino dos Santos Junior <edsonascendino@MacBook-Pro-de-Edson.local>
2023-10-26 23:30:22 -0300

    feature: Adding modules declarations and providers, minor css rule, and bootstrap navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InvestmentCalculatorComponent } from './investment-calculator/investment-calculator.component';
+import { B3OnlyIntegerDirective } from './utils/integer-input.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InvestmentCalculatorComponent,
+    B3OnlyIntegerDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
